130d8e5e0e8d6a075f2a46ed1662819345a93fc5
Andrey Filipenkov <decapitator@ukr.net>
2025-02-21 22:34:28 +0300

    1.6.6

--- Casks/vcmi.rb
+++ Casks/vcmi.rb
@@ -1,9 +1,9 @@
 cask "vcmi" do
   arch arm: "arm", intel: "intel"
 
-  version "1.6.5"
-  sha256 arm:   "6cf91722b037ed9e3677d442186b8e7d4d76dda962c5360c849e4f065c9079ec",
-         intel: "e172bf5c46c57bc6d59603415184e1b3f97e2508e470336cfbf73f924de6f818"
+  version "1.6.6"
+  sha256 arm:   "45c385db2aa0bc40fd964c27205584fa7b0a0e684eade91170c3d5539cd708cd",
+         intel: "fe75b9e32cae9bd4c20b8625638c93205c269a9803ed5edbd64a2d6617c3fee1"
 
   url "https://github.com/vcmi/vcmi/releases/download/#{version}/VCMI-macOS-#{arch}.dmg",
       verified: "github.com/vcmi/vcmi/"
